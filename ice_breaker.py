b43579dc11435116708298037e97e93c875b620c
Kuchar Kal Namish <Namish@Kuchars-MacBook-Air-2.local>
2024-04-03 22:54:13 +0530

    Fetching information for Linkedin Profile API

--- ice_breaker.py
+++ ice_breaker.py
@@ -3,20 +3,12 @@ from langchain.chains.llm import LLMChain
 from langchain_core.prompts import PromptTemplate
 from langchain_google_genai import ChatGoogleGenerativeAI
 
+from third_parties.linkedin import scrape_linkedin_data
 
 if __name__ == "__main__":
     load_dotenv()
 
-    information = """
-        Mahendra Singh Dhoni (/məˈheɪndrə ˈsɪŋ dhæˈnɪ/ ⓘ; born 7 July 1981) is an Indian professional cricketer. 
-        He is a right handed batter and a wicket-keeper. Widely regarded as one of the most prolific 
-        wicket-keeper-batsmen and captains, he represented the Indian cricket team and was the captain of the side in 
-        limited-overs formats from 2007 to 2017 and in test cricket from 2008 to 2014. Dhoni has captained the most 
-        international matches and is the most successful Indian captain. He has led India to victory in the 2011 Cricket 
-        World Cup, the 2007 ICC World Twenty20 and the 2013 ICC Champions Trophy, the only captain to win three 
-        different limited overs tournaments. He also led the teams that won the Asia Cup in 2010, 2016 and was a member 
-        of the title winning squad in 2018.
-    """
+    information = scrape_linkedin_data(profile_url="")
 
     prompt_template = """
         Given the information {information} about a person. I want you to create:
@@ -32,4 +24,4 @@ if __name__ == "__main__":
 
     result = chain.invoke(input={"information": information})
 
-    print(result['text'])
+    print(result["text"])
---
+++ third_parties/linkedin.py
@@ -0,0 +1,42 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+API_KEY = os.environ.get("RAPIDAPI_LINKEDIN_API_KEY")
+
+
+def scrape_linkedin_data(profile_url="https://www.linkedin.com/in/williamhgates/"):
+    # url = "https://fresh-linkedin-profile-data.p.rapidapi.com/get-linkedin-profile"
+    # querystring = {"linkedin_url": profile_url, "include_skills": "true"}
+    #
+    # headers = {
+    #     "X-RapidAPI-Key": API_KEY,
+    #     "X-RapidAPI-Host": "fresh-linkedin-profile-data.p.rapidapi.com"
+    # }
+    #
+    # response = requests.get(url, headers=headers, params=querystring)
+
+    response = requests.get(
+        "https://gist.githubusercontent.com/kknamish/d7aba74a78fcbc31399af146de9bc125/raw/bb48cdfee4ec993d653fee15a437ce824ae6799d/linkedin_profile.json"
+    )
+    resp_data = response.json()["data"]
+    profile = {
+        i: resp_data[i]
+        for i in resp_data
+        if resp_data[i]
+        not in [
+            None,
+            "",
+            [],
+            "urn",
+            "company_logo_url",
+            "company_website",
+            "company_linkedin_url",
+        ]
+    }
+
+    return profile
+
+
+scrape_linkedin_data()
